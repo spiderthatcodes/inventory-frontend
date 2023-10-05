16cff24767ec911623243e6298ceecd6b2d9b6d8
spiderthatcodes <jessicaldickerson@outlook.com>
2023-10-05 10:22:52 -0700

    added google books api

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import './App.css';
 import Nav from './components/Nav/Nav';
-import { GOOGLE_BOOKS_API_KEY } from '../secrets'
+import { GOOGLE_BOOKS_API_KEY } from '../secrets';
+import axios from 'axios';
 
 const App: FC = () => {
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [searchResult, setSearchResult] = useState<[]>([])
+    console.log(searchQuery);
+
     useEffect(() => {
         console.log('blah');
     }, []);
 
-    const example = GOOGLE_BOOKS_API_KEY
+    const searchBooks = () => {
+        const result = searchQuery.split(' ').join('+');
+        axios
+            .get(
+                `https://www.googleapis.com/books/v1/volumes?q=${result}&key=${GOOGLE_BOOKS_API_KEY}`
+            )
+            .then(({data}) => setSearchResult(data.items))
+            .catch((err) => console.log(err));
+    };
 
-    console.log(example)
+    console.log(searchResult)
 
     return (
         <div id='top-level'>
             <Nav />
+            <input
+                type='text'
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button onClick={() => searchBooks()}>Search</button>
         </div>
     );
-}
+};
 
 export default App;
