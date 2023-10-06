a06e5e57a6b58266b8244ea204b790075ef10c42
spiderthatcodes <jessicaldickerson@outlook.com>
2023-10-06 15:11:23 -0700

    pulling in book list, removed auth for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import './App.css';
 import Nav from './components/Nav/Nav';
 import BookModal from './components/BookModal/BookModal';
-import NoAuth from './pages/NoAuth';
+// import NoAuth from './pages/NoAuth';
 import Books from './pages/Books';
+// import axios from 'axios';
 
 const App: FC = () => {
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [searchResult, setSearchResult] = useState<[]>([]);
     const [showModal, setShowModal] = useState<boolean>(false);
     const [bookToShow, setBookToShow] = useState();
-    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    // const [loggedIn, setLoggedIn] = useState<boolean>(true);
 
 
 
-    useEffect(() => {
-        // this is going to pull in the existing lists eventually
-        console.log('blah');
-    }, []);
 
     return (
         <div id='top-level'>
@@ -29,7 +26,9 @@ const App: FC = () => {
                 setShowModal={setShowModal}
             />
 
-            {loggedIn ? <Books /> :<NoAuth setLoggedIn={setLoggedIn} />}
+            <Books />
+
+            {/* {loggedIn ? <Books /> : <NoAuth setLoggedIn={setLoggedIn} />} */}
 
             {showModal && (
                 <BookModal
--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,10 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { FC } from 'react';
+import axios from 'axios';
+import { FC, useEffect, useState } from 'react';
 
 const Books: FC = () => {
-    return (
-      <></>
-    );
+    const [usersBooks, setUsersBooks] = useState<[]>([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:8000/api/books/')
+            .then(({ data }) => setUsersBooks(data))
+            .catch((err) => console.log(err));
+    }, []);
+
+    console.log(usersBooks)
+
+    return <></>;
 };
 
 export default Books;
