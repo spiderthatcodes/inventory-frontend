701a25c5b0b995466fbe62d78d8c3a5a5b2edb5f
spiderthatcodes <jessicaldickerson@outlook.com>
2023-10-08 12:33:58 -0700

    add book works

--- src/components/BookModal/BookModal.tsx
+++ src/components/BookModal/BookModal.tsx
@@ -2,6 +2,7 @@
 // import axios from 'axios'
 import { FC } from 'react';
 import { BookCard, ModalContainer } from './style';
+import axios from 'axios';
 
 const BookModal: FC<{
     book?: any;
@@ -9,15 +10,28 @@ const BookModal: FC<{
     setBookToShow: any;
     setShowModal: any;
 }> = ({ book, searchResult, setBookToShow, setShowModal }) => {
-    // const saveBook = () => {
-    //     axios.post()
-    // }
+    const saveBook = () => {
+        const bookToAdd = {
+            title: book.title,
+            author: book.authors[0],
+            description: book.description,
+            status: 'read',
+            published_on: book.publishedDate,
+            genre: book.categories[0],
+            cover_image: book.imageLinks.thumbnail,
+        };
+        axios
+            .post('http://localhost:8000/api/books/', bookToAdd)
+            .then(({ data }) => console.log(data))
+            .catch((err) => console.log(err));
+    };
 
     return (
         <ModalContainer>
             {book ? (
                 <div>
-                    <h1>{book.title}</h1>
+                    <h1>{book?.authors[0]}</h1>
+                    <button onClick={() => saveBook()}>Add Book</button>
                 </div>
             ) : (
                 <>
@@ -29,9 +43,10 @@ const BookModal: FC<{
                                         book.volumeInfo.imageLinks?.thumbnail !=
                                         undefined
                                 )
-                                .map((book: any, index: number) =>
+                                .map((book: any, index: number) => (
                                     <BookCard key={index}>
-                                        <h1 id='title'
+                                        <h1
+                                            id='title'
                                             onClick={() => {
                                                 setBookToShow(book.volumeInfo);
                                                 setShowModal(true);
@@ -48,7 +63,7 @@ const BookModal: FC<{
                                         />
                                         <div id='details'></div>
                                     </BookCard>
-                                )}
+                                ))}
                         </>
                     )}
                 </>
